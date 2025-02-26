519fb029785f045253605ecd0170a17a02ad4ad8
Debbie012 <ntigelidebbie@gmail.com>
2025-02-26 22:10:38 +0100

    update

--- Python Playground.py
+++ Python Playground.py
@@ -3,3 +3,26 @@ print("Five is greater than two!")
 print("Comment")
 #This is a comment
 print("Hello World")
+print("Hello World!") #This is a comment.
+#print("Hello World")
+print("Cheers, Mate!")
+x = 5
+y = "John"
+print(x)
+print(y)
+x = 4    #x is of type int
+x = "Sally"   #x is now of type str
+print(x)
+x = str(3)
+y = int(3)
+z = float(3)
+print(x)
+print(y)
+print(z)
+x = 6
+y = "John"
+print(type(x))
+print(type(y))
+
+
+
