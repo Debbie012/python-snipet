e27d630b0ce8930bd5830a40d97ce5b35d0e1b6c
Debbie Ntigeli <ntigelidebbie@gmail.com>
2025-02-28 23:11:31 +0100

    update

--- Python Playground.py
+++ Python Playground.py
@@ -297,9 +297,72 @@ print(x)
 #display the data type of x
 print(type(x))
 
+#Python Numbers
+x = 1
+y = 2.8
+z = 1j
 
+print(type(x))
+print(type(y))
+print(type(z))
 
+#Int, or integer, is a whole number, positive or negative, without decimals, of unlimited length.
+x = 1
+y = 456345675
+z = -453467
 
+print(type(x))
+print(type(y))
+print(type(z))
+
+#Float, or "floating point number" is a number, positive or negative, containing one or more decimals.
+x = 1.10
+y = 1.0
+z = -34.56
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#Float can also be scientific numbers with an "e" to indicate the power of 10.
+x = 34e6
+y = 12E4
+z = -87.6e100
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#Complex numbers are written with a "j" as the imaginary part
+x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#You can convert from one type to another with the int(), float(), and complex() methods:
+##convert from int to float
+x = float(1)
+
+#convert from float to int
+y = int(2.5)
+
+#convert from int to complex
+z = complex(1)
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#Python has a built-in module called random that can be used to make random numbers
+import random
+print(random.randrange(1, 10))
 
 
 
