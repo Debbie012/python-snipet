e37138fc55d8f268ddbe8a731688179717cc6fa4
Debbie Ntigeli <ntigelidebbie@gmail.com>
2025-02-28 23:23:53 +0100

    update

--- Python Playground.py
+++ Python Playground.py
@@ -364,6 +364,36 @@ print(type(z))
 import random
 print(random.randrange(1, 10))
 
+#Specifying variables
+#Integers
+x = int(1)
+y = int(2.8)
+z = int("3")
+
+print(x)
+print(y)
+print(z)
+
+#Floats
+x = float(1)
+y = float(2.8)
+z = float("3")
+w = float("4.2")
+
+print(x)
+print(y)
+print(z)
+print(w)
+
+#Strings
+x = str("s1")
+y = str(2)
+z = str(3.0)
+
+print(x)
+print(y)
+print(z)
+
 
 
 
